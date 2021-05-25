8337385ad7912467923a37bfc17737e1d564cde4
arina kuznetsova <ieroarina@gmail.com>
2021-05-25 16:50:52 +0300

    front first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = 3088;
+
+app.use(express.static(path.join(__dirname ,'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+app.get('/guest', function(req, res){
+    res.render('scheduleAdmin.ejs');
+});
+app.listen(PORT, ()=>{
+    console.log('Server Start!!!!')
+});
