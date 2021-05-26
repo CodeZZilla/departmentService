a8c4d5dfbd209869d78c67844eb233cef248383e
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-05-26 09:25:31 +0300

    ебать

--- index.js
+++ index.js
@@ -17,28 +17,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const mainRouter = require('./routes/mainRouter.js')
-
+const guestRouter = require('./routes/guestRouter.js')
 
 app.use('/', mainRouter);
+app.use('/guest', guestRouter)
+
+app.get('/guest', function(req, res){
+    res.render('guest.ejs');
+});
 
 app.use(function (req, res, next) {
     res.status(404).render('page404')
 });
-<<<<<<< HEAD
-app.get('/404', function(req, res){
-    res.render('404.ejs');
-});
-=======
 // app.get('/', function(req, res){
 //     res.render('index');
 // });
-// app.get('/guest', function(req, res){
-//     res.render('guest.ejs');
-// });
+
 // app.get('/admin', function(req, res){
 //     res.render('scheduleAdmin.ejs');
 // });
->>>>>>> 4b0c0aecb114aed0ce88ed269ea1edc3febd345e
 app.listen(PORT, ()=>{
     console.log('Server Start!!!!')
 });
