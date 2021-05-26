db746aef6172e77468f91d4624b82bd77c948269
Mark Shulmeyster <markshulmeyster@gmail.com>
2021-05-26 08:58:48 +0300

    хуета

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
+'use strict'
 const express = require('express');
-const path = require('path');
-
-
+const path = require("path");
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+// const passport = require('./config/passport');
+// const connectEnsureLogin = require('connect-ensure-login');
 const app = express();
+const flash = require('express-flash');
+
+// app.use(flash());
+
 const PORT = 3088;
 
-app.use(express.static(path.join(__dirname ,'public')));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('index');
+const mainRouter = require('./routes/mainRouter.js')
+
+
+app.use('/', mainRouter);
+
+app.use(function (req, res, next) {
+    res.status(404).render('page404')
 });
-app.get('/guest', function(req, res){
-    res.render('guest.ejs');
-});
-app.get('/admin', function(req, res){
-    res.render('scheduleAdmin.ejs');
-});
+// app.get('/', function(req, res){
+//     res.render('index');
+// });
+// app.get('/guest', function(req, res){
+//     res.render('guest.ejs');
+// });
+// app.get('/admin', function(req, res){
+//     res.render('scheduleAdmin.ejs');
+// });
 app.listen(PORT, ()=>{
     console.log('Server Start!!!!')
 });
---
+++ routes/mainRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mainController = require('../controllers/mainController.js');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+const mainRouter = express.Router();
+
+mainRouter.get('/', mainController.getAll);
+// mainRouter.post('/login', urlencodedParser, mainController.login)
+
+module.exports=mainRouter;
