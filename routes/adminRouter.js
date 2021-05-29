8db0efe8ea852c9539ea89fe5858d7d917a4a24c
Dima <dimmaa9@gmail.com>
2021-05-29 14:08:09 +0300

    new

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -44,7 +44,10 @@ adminRouter.get('/logout', adminController.logOut);
 adminRouter.post('/addDiscipline',urlencodedParser, adminController.addDiscipline);
 adminRouter.post('/deleteDiscipline',urlencodedParser, adminController.deleteDiscipline)
 adminRouter.get('/getAll2', adminController.getAll2);
+adminRouter.get('/getAllGroups', adminController.getAllGroups);
 adminRouter.post('/addGroup', adminController.addGroup)
 adminRouter.post('/deleteGroup', adminController.deleteGroup)
+
+adminRouter.get('/discipline/:id', adminController.getPageDiscipline);
 
 module.exports=adminRouter;
