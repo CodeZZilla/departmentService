b70ae0f80f197cd6c958bd22e2ea8d4357eb23f8
3VlaDIslaV3 <bublik@bezsalu.pp.ua>
2021-05-29 20:29:52 +0300

    teacherRout +

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,3 +1,19 @@
+const Teachers= require('../models/Teachers')
 exports.getAll = function (req,res){
     res.render('teacher');
+};
+
+exports.logOut = function (req, res, next) {
+    req.logout();
+
+    res.clearCookie('keyboard cat' , {path:'/'});
+
+    req.session.destroy(function (err) {
+       if (err)
+           return next(err);
+
+       req.session = null;
+
+       res.redirect('/');
+    });
 };
